94de470a79f8ccaec08fd28ef9a34135e1837f81
cassie.cattabiani@gmail.com <cassie.cattabiani@gmail.com>
2020-06-14 11:10:57 -0700

    added fucntion for upper lower number and symbol

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,38 @@
 // Assignment code here
 
+//Get random lower case function
+
+function getRandomLower () {
+  const lower = 'abcdefghijklmnopqrstuvwxyz'
+  return lower[Math.floor(Math.random() * lower.length)];
+}
+console.log(getRandomLower());
+
+//Get random upper case function
+
+function getRandomUpper () {
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  return upper[Math.floor(Math.random() * upper.length)];
+}
+console.log(getRandomUpper());
+
+//Get random numeric value function
+
+function getRandomNumber () {
+  const number = '0123456789'
+  return number[Math.floor(Math.random() * number.length)];
+}
+console.log(getRandomNumber());
+
+//Get random symbols function
+
+function getRandomSymbol () {
+  const symbols = '!@#$%^&*~<>;/';
+  return symbols[Math.floor(Math.random() * symbols.length)]; 
+}
+console.log(getRandomSymbol());
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
